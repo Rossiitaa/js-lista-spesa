e74d51341f972a302c128576332dbb6d08ee2257
Rossiitaa <rosscuotto1998@gmail.com>
2022-06-21 15:29:34 +0200

    exercise finished

--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
-const shoppingList = ['Wurstel', 'Latte', 'Uova', 'Pasta', 'Pomodori','Mozzarella', 'Fagioli']
+let shoppingList = ['Wurstel', 'Latte', 'Uova', 'Pasta', 'Pomodori','Mozzarella', 'Fagioli'];
+
+const list = document.querySelector('ul', '#shopping-list');
+let i = 0;
+
+while (i < shoppingList.length) {
+
+    let element = shoppingList[i];
+    let elementList = document.createElement('li');
+    elementList.innerText = element;
+    list.append(elementList);
+    
+    i++
+}
